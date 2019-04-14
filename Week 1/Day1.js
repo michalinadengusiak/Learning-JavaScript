cafcecd468ea9f75794d2018c912a2553360dfcb
Michalina Dengusiak <michalina.dengusiak@gmail.com>
2019-04-14 14:27:20 +0200

    started with day1 file

---
+++ Week 1/Day1.js
@@ -0,0 +1,19 @@
+
+////Basic JavaScript
+
+var console;
+var alert;
+
+
+var FirstName = 'John';
+var LastName = 'Smith';
+var Age = 28;
+var Job = 'teacher';
+console.log(FirstName + ' ' + LastName + ' is a ' + Age + ' year old ' + Job);
+
+var firstName, lastName, age, job;
+firstName = 'Samantha';
+lastName = 'Jones';
+age = 27;
+job = 'driver';
+alert(firstName + ' ' + lastName + ' is a ' + age + ' year old ' + job);
